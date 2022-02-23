cf107dd46431502639cc61dfddc9b8d2674f5b5c
Sanket Supekar <sanketsupekar123@gmail.com>
2022-02-23 18:43:21 +0530

    Social Account Update

--- src/components/SocialLink.js
+++ src/components/SocialLink.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faYoutube, faLinkedin, faGithub, faHackerrank, faInstagram,faFacebook} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons"
+import { faYoutube, faLinkedin, faGithub, faInstagram,faFacebook} from "@fortawesome/free-brands-svg-icons";
 import '../App';
 
 
@@ -10,21 +11,24 @@ export default function () {
       <a href="https://www.linkedin.com/in/sanketsupekar" className="btn btn-light m-3">
         <FontAwesomeIcon icon={faLinkedin} />
       </a>
+
+      <a href="mailto:sanketsupekar123@gmail.com" className="btn btn-light m-3">
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
+
       <a href="https://github.com/sanketsupekar" className="btn btn-light m-3">
         <FontAwesomeIcon icon={faGithub} />
       </a>
 
-      <a href="https://www.hackerrank.com/sanketsupekar" className="btn btn-light m-3">
-        <FontAwesomeIcon icon={faHackerrank} />{" "}
-      </a>
-      <a href="https://www.youtube.com/channel/UCsZS_PHRWkho8l6u8jR2nhg" className="btn btn-light m-3">
-        <FontAwesomeIcon icon={faYoutube} />{" "}
-      </a>
       <a href="https://www.instagram.com/sanket_supekar_patil/" className="btn btn-light m-3">
         <FontAwesomeIcon icon={faInstagram} />{" "}
       </a>
       <a href="https://www.facebook.com/sanket.supekar.712/" className="btn btn-light m-3">
         <FontAwesomeIcon icon={faFacebook} />{" "}
+      </a>
+
+      <a href="https://www.youtube.com/channel/UCsZS_PHRWkho8l6u8jR2nhg" className="btn btn-light m-3">
+        <FontAwesomeIcon icon={faYoutube} />{" "}
       </a>
 
     </div>
