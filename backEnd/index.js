cd2012994defc06475f18fa1053ffe8671ade0bf
Sanket Supekar <sanketsupekar123@gmail.com>
2023-02-10 14:15:27 +0530

    update for cloud

---
+++ backEnd/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const connectToDb = require("./controllers/mongoose.controller");
+const apiRouter = require("./routes");
+const path = require("path");
+const Contact = require("./model/contact.model");
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
+
+connectToDb();
+app.use("/api", apiRouter);
+app.use(express.static(path.resolve(__dirname, "../frontEnd/build")));
+app.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontEnd/build", "index.html"));
+});
+app.listen(port, () => {
+  console.log("Server is ready to listening....",port);
+});
